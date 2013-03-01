b12f50354613f1c47d5242c1616094218d5d381a
abrkn <a@abrkn.com>
2013-03-01 20:21:52 +0700

    add multi table support

--- bin/game.js
+++ bin/game.js
@@ -6,13 +6,20 @@ var debug = require('debug')('bunny:game')
 , ref = new Firebase(config.firebase.url)
 , generator = new FirebaseTokenGenerator(config.firebase.secret)
 , token = generator.createToken(null, { admin: true, debug: false })
+, tables = []
 
 debug('authenticating with firebase')
 
 ref.auth(token, function(error, response) {
     if (error) throw error
     debug('authenticated with firebase')
-    debug('setting up table #0')
-    var tableRef = ref.child('tables/0')
-    , table = new Table(tableRef)
+
+    ref.child('tables').once('value', function(snapshot) {
+        snapshot.forEach(function(snapshot) {
+            debug('setting up table #%s', snapshot.name())
+            var tableRef = ref.child('tables/' + snapshot.name())
+            , table = new Table(tableRef)
+            tables.push(table)
+        })
+    })
 })
